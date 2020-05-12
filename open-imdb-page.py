77f600db1973d23dc953b131dc238deea3450d13
ctlaltdefeat <jgradstein@gmail.com>
2020-05-12 17:35:56 +0300

    improve performance for multi-season series

--- open-imdb-page.py
+++ open-imdb-page.py
@@ -1,31 +1,43 @@
-from imdb import IMDb
-from guessit import guessit
 import sys
 import webbrowser
+
+from guessit import guessit
+from imdb import IMDb
+
 
 def get_imdb_url(filename):
     result = None
     ia = IMDb()
     g = guessit(filename)
-    t = g['title']
-    if 'year' in g:
-        t = "{} {}".format(t, g['year'])
-    results = ia.search_movie(t)
-    if g['type'] == 'episode':
-        it = iter(res for res in results if res['kind'] == 'tv series')
+    title = g["title"]
+    if "year" in g:
+        title = "{} {}".format(title, g["year"])
+    results = ia.search_movie(title)
+    if g["type"] == "episode":
+        it = iter(res for res in results if res["kind"] == "tv series")
         while not result:
             series = next(it)
-            ia.update(series, "episodes")
-            if (g['season'] in series['episodes']) and (g['episode'] in series['episodes'][g['season']]):
-                result = series['episodes'][g['season']][g['episode']]
+            season = g["season"]
+            try:
+                ia.update_series_seasons(series, [season])
+                result = series["episodes"][season][g["episode"]]
+            except Exception:
+                pass
     else:
-        result = next(iter(res for res in results if res['kind'] not in ['tv series', 'episode']))
+        result = next(
+            iter(
+                res
+                for res in results
+                if res["kind"] not in ["tv series", "episode"]
+            )
+        )
     return ia.get_imdbURL(result)
+
 
 if __name__ == "__main__":
     try:
         url = get_imdb_url(sys.argv[1])
         webbrowser.open_new_tab(url)
-    except:
-        sys.stderr.write('Failed to find IMDb URL')
+    except Exception as e:
+        print("ERROR: {}".format(e))
         sys.exit(1)
