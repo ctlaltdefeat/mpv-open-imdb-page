d5959b6bf112cbcc35c21d7a4525c8c86b7bab33
Till <till.runge@gmail.com>
2022-02-12 12:27:43 +0100

    add python binary search
    - adds support for Ubuntu
    - tries python and python3
    - checks python version
    - displays error message, if python 3 is not found

--- main.lua
+++ main.lua
@@ -1,3 +1,41 @@
+function get_python_binary()
+    local msg = ""
+
+    -- try: python
+    local python_version = mp.command_native({
+        name = "subprocess",
+        args = { "python", "--version"},
+        capture_stdout = true
+    })
+    if python_version.error_string ~= "" then
+        msg = msg.."'python' not found; "
+    else
+        if python_version.stdout:find("3%.") ~= nil then
+            msg = msg.."'python' with version 3 found!"
+            return "python", msg
+        else
+            msg = msg.."'python' is not version 3; "
+        end
+    end
+    
+    -- try: python3
+    python_version = mp.command_native({
+        name = "subprocess",
+        args = { "python3", "--version" },
+        capture_stdout = true
+    })
+    if python_version.error_string ~= "" then
+        msg = msg.."'python3' not found; "
+    else
+        msg = msg.."'python3' found!"
+        return "python3", msg
+    end
+
+    --no python 3 found
+    msg = msg.."no Python 3 binary found!"
+    return nil, msg
+end
+
 function callback(success, result, error)
     if result.status == 0 then
         mp.osd_message("Launched browser", 1)
@@ -7,15 +45,24 @@ function callback(success, result, error)
 end
 
 function launch_imdb()
-    mp.osd_message("Finding IMDb URL...", 30)
-    local py_script_path = mp.get_script_directory().."/open-imdb-page.py"
-    local cmd = mp.command_native_async({
-            name = "subprocess",
-            args = {"python", py_script_path, mp.get_property("filename")}
-        },
-        callback
-    )
+    local python_binary, python_msg = get_python_binary()
+    if python_binary ~= nil then
+        mp.osd_message("Finding IMDb URL...", 30)
+        local cmd = mp.command_native_async({
+                name = "subprocess",
+                args = {
+                    python_binary,
+                    mp.get_script_directory().."/open-imdb-page.py",
+                    mp.get_property("filename")
+                }
+            },
+            callback
+        )
+    else
+       mp.osd_message("ERROR: "..python_msg, 10)
+       return
+    end
 end
 
--- change key binding as desired 
+-- change key binding as desired
 mp.add_key_binding('ctrl+i', 'launch-imdb', launch_imdb)
